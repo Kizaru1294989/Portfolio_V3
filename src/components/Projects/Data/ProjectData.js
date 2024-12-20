e6b5568232f498ba0e41c8dd9af44fa8f572c05d
ryan <ryan.rais@ynov.com>
2024-12-20 17:16:42 +0100

    projects

---
+++ src/components/Projects/Data/ProjectData.js
@@ -0,0 +1,25 @@
+
+
+
+export const ProjectData = {
+    Arista : {
+        description : "",
+        link : "",
+        img_path : ""
+    },
+    DC:{
+        description : "",
+        link : "",
+        img_path : ""
+    },
+    Website : {
+        description : "",
+        link : "",
+        img_path : ""
+    },
+    ELK:{
+        description : "",
+        link : "",
+        img_path : ""
+    }
+}
